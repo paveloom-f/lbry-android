810bffdfa8703e665d552a3ef770e19d0824252f
Pavel Sobolev <paveloom@tuta.io>
2021-09-12 14:51:02 +0300

    Fix a typo in the `sed` call.

--- .github/scripts/build_a_release.bash
+++ .github/scripts/build_a_release.bash
@@ -13,7 +13,7 @@ android {
 }" >> app/build.gradle
 
 # Disable the filter
-sed -i 's|if (Helper.isClaimBlocked(claim))|if (false)/g' $(grep -lr "if (Helper.isClaimBlocked(claim))" app/src)
+sed -i 's|if (Helper.isClaimBlocked(claim))|if (false)|g' $(grep -lr "if (Helper.isClaimBlocked(claim))" app/src)
 
 # Build APKs
 chmod +x gradlew
